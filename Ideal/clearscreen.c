29ff29f814f1b19f614704b161dfaf6064731844
RiZuRuZ <poori.rigi@gmail.com>
2025-11-05 09:27:50 +0700

    commit

---
+++ Ideal/clearscreen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello!\n");
+    //system("pause");  // รอให้กดปุ่มก่อนเคลียร์
+    system("cls");    // เคลียร์หน้าจอ
+    printf("Screen cleared!\n");
+    return 0;
+}
